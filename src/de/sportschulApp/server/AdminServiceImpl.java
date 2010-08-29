d9c5780690e828fd7537326e5c3e8a6d60f47b33
Theonlybender <Theonlybender@t-online.de>
2010-08-29 14:17:48 +0200

    memberlistview update

--- src/de/sportschulApp/server/AdminServiceImpl.java
+++ src/de/sportschulApp/server/AdminServiceImpl.java
@@ -1,12 +1,23 @@
 package de.sportschulApp.server;
 
+import java.util.ArrayList;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.sportschulApp.client.services.AdminService;
+import de.sportschulApp.server.databanker.DataBankerMember;
+import de.sportschulApp.shared.Member;
 
 @SuppressWarnings("serial")
 public class AdminServiceImpl extends RemoteServiceServlet implements
 		AdminService {
+	
+	DataBankerMember dbm = new DataBankerMember();
+
+	public ArrayList<Member> getMemberList() {
+		ArrayList<Member> memberList = dbm.getMemberList();
+		return memberList;
+	}
 
 	
 }
