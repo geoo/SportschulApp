d9947313db2851b13cfb615fc84461ee10341454
geoo <geo@nukular-pictures.de>
2010-09-20 20:20:35 +0200

    trainer update

--- src/de/sportschulApp/client/services/TrainerServiceAsync.java
+++ src/de/sportschulApp/client/services/TrainerServiceAsync.java
@@ -2,7 +2,10 @@ package de.sportschulApp.client.services;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import de.sportschulApp.shared.Member;
 import de.sportschulApp.shared.User;
 
 public interface TrainerServiceAsync {
+
+	void getMemberByBarcodeID(int barcodeID, AsyncCallback<Member> callback);
 }
--- src/de/sportschulApp/server/TrainerServiceImpl.java
+++ src/de/sportschulApp/server/TrainerServiceImpl.java
@@ -3,10 +3,17 @@ package de.sportschulApp.server;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.sportschulApp.client.services.TrainerService;
+import de.sportschulApp.server.databanker.DataBankerMember;
+import de.sportschulApp.shared.Member;
 
 @SuppressWarnings("serial")
 public class TrainerServiceImpl extends RemoteServiceServlet implements
 		TrainerService {
 
-	
+	DataBankerMember dbm = new DataBankerMember();
+
+	public Member getMemberByBarcodeID(int barcodeID) {
+		return dbm.getMember(barcodeID);
+	}
+
 }
